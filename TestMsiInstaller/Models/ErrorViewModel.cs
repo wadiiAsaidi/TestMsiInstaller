8d42b3bba6f52c7eedca0c24464ef39c28ba07d4
WADII\wadii <wadii.alsaidi@ensi-uma.tn>
2024-09-06 21:04:51 +0100

    Add project files.

---
+++ TestMsiInstaller/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestMsiInstaller.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
